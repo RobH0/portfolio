bc8ce673c76258423c2bb8bfe895ef087d61c507
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-15 13:20:52 -0700

    Added content to Hero component.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,8 @@
 import styles from './HeroStyles.module.css';
 import profileImg from '../assets/profilePic.jpg'
+import githubIcon from '../assets/icons8-github.svg'
+import linkedinIcon from '../assets/icons8-linkedin.svg'
+import arrow from '../assets/down-arrow-5-svgrepo-com.svg'
 
 function Hero() {
   return (
@@ -8,8 +11,26 @@ function Hero() {
             <img src={profileImg} alt="Robert's Profile Picture"/>
         </div>
         <div className='hero-text'>
-            <h1>Robert Holdsworth</h1>
+            <h1>
+                Robert
+                <br />
+                Holdsworth
+            </h1>
             <h2>FullStack Developer</h2>
+            <div className={styles.socials}>
+                <a href="https://github.com/RobH0" target='_blank'>
+                    <img src={githubIcon} alt="GitHub Icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/robert-holdsworth-771911148/" target='_blank'>
+                    <img src={linkedinIcon} alt="Linkedin Icon" />
+                </a>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, vel quis laboriosam ex placeat esse obcaecati. Porro non molestiae animi!
+            </p>
+        </div>
+        <div class="arrow-bounce">
+            <img src={arrow} alt="Arrow symbol pointing downwards" />
         </div>
     </section>
   )
