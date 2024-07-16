a572bd34913d98ab42b6dff19ec9759f68266ff3
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-15 21:47:48 -0700

    Added styling to Hero component.

---
+++ src/interactive/ArrowComponent.jsx
@@ -0,0 +1,10 @@
+import arrow from '../assets/down-arrow-5-svgrepo-com.svg'
+import styles from './ArrowStyles.module.css'
+
+export default function ArrowComponent() {
+  return (
+    <div className={styles.container}>
+      <button className={styles.btn}><img src={arrow} alt="Arrow symbol pointing downwards" /></button>
+    </div>
+  )
+}
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,37 +1,38 @@
 import styles from './HeroStyles.module.css';
-import profileImg from '../assets/profilePic.jpg'
-import githubIcon from '../assets/icons8-github.svg'
-import linkedinIcon from '../assets/icons8-linkedin.svg'
-import arrow from '../assets/down-arrow-5-svgrepo-com.svg'
+import profileImg from '../assets/profilePic.jpg';
+import githubIcon from '../assets/icons8-github.svg';
+import linkedinIcon from '../assets/icons8-linkedin.svg';
+import ArrowComponent from '../interactive/ArrowComponent.jsx';
+
 
 function Hero() {
   return (
-    <section className='hero-section'>
-        <div className=''>
-            <img src={profileImg} alt="Robert's Profile Picture"/>
-        </div>
-        <div className='hero-text'>
-            <h1>
-                Robert
-                <br />
-                Holdsworth
-            </h1>
-            <h2>FullStack Developer</h2>
-            <div className={styles.socials}>
-                <a href="https://github.com/RobH0" target='_blank'>
-                    <img src={githubIcon} alt="GitHub Icon" />
-                </a>
-                <a href="https://www.linkedin.com/in/robert-holdsworth-771911148/" target='_blank'>
-                    <img src={linkedinIcon} alt="Linkedin Icon" />
-                </a>
+    <section>
+        <div className={styles.heroSection}>
+            <div className={styles.profileDiv}>
+                <img src={profileImg} alt="Robert's Profile Picture"/>
             </div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, vel quis laboriosam ex placeat esse obcaecati. Porro non molestiae animi!
-            </p>
+            <div className={styles.heroText}>
+                <h1>
+                    Robert
+                    <br />
+                    Holdsworth
+                </h1>
+                <h2>FullStack Developer</h2>
+                <div className={styles.socials}>
+                    <a href="https://github.com/RobH0" target='_blank'>
+                        <img src={githubIcon} alt="GitHub Icon" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/robert-holdsworth-771911148/" target='_blank'>
+                        <img src={linkedinIcon} alt="Linkedin Icon" />
+                    </a>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, vel quis laboriosam ex placeat esse obcaecati. Porro non molestiae animi!
+                </p>
+            </div>
         </div>
-        <div class="arrow-bounce">
-            <img src={arrow} alt="Arrow symbol pointing downwards" />
-        </div>
+        <ArrowComponent />
     </section>
   )
 }
