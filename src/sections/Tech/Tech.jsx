0c71d15aa00752bc1d3ac28ae9a40ac24ff80fdf
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-18 19:36:47 -0700

    style: made Tech section styling responsive for mobile devices

--- src/sections/Tech/Tech.jsx
+++ src/sections/Tech/Tech.jsx
@@ -7,13 +7,13 @@ function Tech() {
         <h1 className='sectionTitle'>Tech</h1>
         <div className={styles.techTypes}>
             <h2>Languages</h2>
-            <div className={styles.typeDiv}>
+            <div className={`${styles.typeDiv} ${styles.evenIcons}`}>
                 <TechIcon name="JavaScript" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
                 <TechIcon name="Python" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
                 <TechIcon name="HTML" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" />
                 <TechIcon name="CSS" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" />
             </div>
-            <h2>Libraries & Frameworks & Runtimes</h2>
+            <h2>Libraries, Frameworks & Runtimes</h2>
             <div className={styles.typeDiv}>
                 <TechIcon name="React" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
                 <TechIcon name="Express.js" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
@@ -26,13 +26,13 @@ function Tech() {
             </div>
             
             <h2>Tools & Other Tech</h2>
-            <div className={styles.typeDiv}>
+            <div className={`${styles.typeDiv} ${styles.evenIcons}`}>
                 <TechIcon name="Git" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
                 <TechIcon name="Nginx" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nginx/nginx-original.svg" />
                 <TechIcon name="Jira" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg" />
                 <TechIcon name="Ollama" source="https://github.com/RobH0/project-gifs/blob/main/ollama.png?raw=true" />
             </div>
-            
+
             <h2>Operating Systems</h2>
             <div className={styles.typeDiv}>
                 <TechIcon name="Linux" source="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" />
