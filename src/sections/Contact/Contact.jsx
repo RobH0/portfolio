69659f1dfc65b5340af15fdbab604f6c8e6e5dcf
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-19 13:42:32 -0700

    feat: Added contact form. Requires styling.

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,37 @@
+import styles from './ContactStyles.module.css';
+
+function Contact() {
+  return (
+    <section id="Contact" className={styles.container}>
+        <h1>Contact Me</h1>
+        <h3>Want to get in touch?</h3>
+        <p>Fill out the form below to reach me via email.</p>
+        <form 
+            method='POST' 
+            name="portfolio-contact" 
+            data-netlify="true" 
+            netlify-honey-post="pass"
+            data-netlify-recaptcha="true"
+            className={styles.contactForm}
+            >
+            <input type="hidden" name="pass"/>
+            <div className={styles.formDiv}>
+                <label name="name" hidden>Name</label>
+                <input id="form-name" type="text" name="name" placeholder='Name' required />
+            </div>
+            <div className={styles.formDiv}>
+                <label name="email" hidden>email</label>
+                <input id="form-email" type="text" name="email" placeholder='email' required />
+            </div>
+            <div className={styles.formDiv}>
+                <label name="message" hidden>Message</label>
+                <textarea id="form-message" type="text" name="message" placeholder='Enter your message here...' required></textarea>
+            </div>
+            <div data-netlify-recaptcha="true"></div>
+            <button type="submit">Submit</button>
+        </form>
+    </section>
+  )
+}
+
+export default Contact
